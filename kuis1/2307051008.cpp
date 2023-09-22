66a7e92652e3342f4d3ebeabfc6e6156328bdc94
vexra <unique123wowros@gmail.com>
2023-09-22 09:03:59 +0700

    kuis 1

---
+++ kuis1/2307051008.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+
+    cout << "Hasil dari " << a << " + " << b << " adalah " << a+b << endl;
+
+    return 0;
+}
