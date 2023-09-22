abe191d5d6150210bd295a72572586fe2cb4423a
vexra <unique123wowros@gmail.com>
2023-09-22 08:32:50 +0700

    kuis

---
+++ kuis1/ananta_2307051001.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// 2307051001 2307051002
+
+int main () {
+    float a,b;
+    cin >> a >> b;
+    cout << "Hasil dARI " << a << " " << b << " adalah " << a+b << endl; 
+    cout << "hasil dari " << a << " " << b << " adalah " << a-b << endl;
+
+
+
+}
+
+
+
+
+
+
---
+++ kuis1/anggun_2307051004.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () 
+{ 
+  float velue1, velue2;
+  cin >> velue1 >> velue2;
+  cout << "Hasil dari penumlahan " << velue1 << " + "  << velue2 << " adalah " << velue1+velue2 << endl;
+  
+}
